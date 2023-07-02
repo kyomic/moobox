321429fd6b1cb064e1fff12cd877272f25bad615
wangxiankun <kyomic@163.com>
2023-07-02 23:11:19 +0800

    feat: fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path ,{resolve} from 'path'
+import path, { resolve } from 'path'
 // npm i fast-glob
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import viteCompression from "vite-plugin-compression";
+
 
 // https://vitejs.dev/config/
 
 
-export default defineConfig({
+const defaultConfig = {
+  build: {
+    minify: "esbuild"
+  },
   plugins: [
     vue(),
     createSvgIconsPlugin({
@@ -32,4 +37,40 @@ export default defineConfig({
       },
     },
   },
+}
+// see:http://www.5ityx.com/cate104/134659.html
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log('mode', mode)
+  console.log('command', command)
+  if (mode == 'library') {
+    console.log("-###")
+    return {
+      ...defaultConfig,
+      build: {
+        ...defaultConfig.build || {},
+        outDir: 'lib',
+        lib: {
+          // Could also be a dictionary or array of multiple entry points
+          entry: resolve(__dirname, 'src/library.ts'),
+          name: '@cookee/moobox',
+          // the proper extensions will be added
+          fileName: 'index',
+        },
+      }
+
+    }
+  }
+  return {
+
+  }
+  if (command === "serve") {
+    return {
+      ...defaultConfig
+    }
+  } else {
+    return defaultConfig
+  }
 })
+
